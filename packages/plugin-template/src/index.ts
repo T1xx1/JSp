4a4e320347f281b8b598f5d7a2794136a0beef25
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-01-20 00:21:43 +0100

    chore(plugin-template): adjust boiletplate

---
+++ packages/plugin-template/src/index.ts
@@ -0,0 +1,6 @@
+export default function ({ types: t }: any) {
+	return {
+		name: '@jsp/plugin-template',
+		visitor: {},
+	};
+}
---
+++ packages/plugin-template/tests/index.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'tester';
+
+test('Baseline', () => {
+	
+});
