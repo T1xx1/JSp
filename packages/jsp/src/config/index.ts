8a53498fa2b6876e60120aa0a3065ad57d6555e9
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-01-23 19:21:36 +0100

    chore: config schema & type

--- packages/jsp/src/config/index.ts
+++ packages/jsp/src/config/index.ts
@@ -1,4 +1,20 @@
 export type Config = {
-	inDir: string;
-	outDir: string;
+	$schema: string;
+	include?: string[];
+	exclude?: string[];
+	compiler?: {
+		emitLang?: 'JavaScript' | 'TypeScript';
+		emitDir?: string;
+	};
+};
+
+export const defaultConfig: Config = {
+	$schema: 'https://github.com/t1xx1/jsp/blob/main/packages/jsp/src/config/$schema.json',
+	include: [
+		'./**'
+	],
+	compiler: {
+		emitLang: 'JavaScript',
+		emitDir: './dist',
+	},
 };
