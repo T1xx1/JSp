8d947eb8f9628e53f8fb80ee876b06d338c9a328
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-01-24 00:29:15 +0100

    chore(jsp): rm node args

--- packages/jsp/src/cli/index.ts
+++ packages/jsp/src/cli/index.ts
@@ -1,9 +1,5 @@
-import process from 'node:process';
-
 import { compileDir } from '../compiler/index.js';
 import { getConfig, mergeConfig } from '../config/index.js';
-
-const args = process.argv.slice(2);
 
 const config = mergeConfig(getConfig() ?? {});
 
