4a3a1f65160a21d310cd49ab45b4789056b60e64
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-01-24 22:19:22 +0100

    feat(jsp): init cmd

--- packages/jsp/src/cli/index.ts
+++ packages/jsp/src/cli/index.ts
@@ -1,12 +1,37 @@
+import { existsSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { cwd, exit } from 'node:process';
+
 import { Command } from 'commander';
 
 import { compileDir } from '../compiler/index.js';
-import { getConfig, mergeConfig } from '../config/index.js';
+import { getConfig, getInitConfig, mergeConfig } from '../config/index.js';
 import { log } from '../utils/index.js';
 
 const cli = new Command();
 
 cli.name('JS+').version('0.1.0');
+
+cli
+	.command('init')
+	.description('init JS+ config')
+	.action(() => {
+		if (getConfig() !== null) {
+			log.info('JS+ config is already initialized');
+
+			exit();
+		}
+
+		let configPath = cwd();
+
+		if (existsSync(join(cwd(), '.config'))) {
+			configPath = join(configPath, '.config/jsp.json');
+		} else {
+			configPath = join(configPath, 'jsp.config.json');
+		}
+
+		writeFileSync(configPath, JSON.stringify(getInitConfig(), null, '\t'), 'utf8');
+	});
 
 cli
 	.command('build')
