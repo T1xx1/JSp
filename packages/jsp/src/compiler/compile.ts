bec772a688af36032ce7d8162cbe68ee739089b5
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-01-24 22:36:09 +0100

    change(jsp): sort plugins

--- packages/jsp/src/compiler/compile.ts
+++ packages/jsp/src/compiler/compile.ts
@@ -9,19 +9,26 @@ export const compile = (jspCode: string, config: CompleteConfig): string => {
 
 	const jsCode = transformSync(jspCode, {
 		plugins: [
-			config.compiler.emitLang === 'TypeScript' ? '@babel/plugin-syntax-typescript' : '@babel/plugin-transform-typescript',
+			/* parse TypeScript */
+			config.compiler.emitLang === 'TypeScript'
+				? '@babel/plugin-syntax-typescript'
+				: '@babel/plugin-transform-typescript',
+
+			/* transform custom syntax */
 			'module:@jsp/plugin-typeof-null-operator',
 			'module:@jsp/plugin-negative-array-subscript',
+			'module:@jsp/plugin-chained-comparisons',
+
+			/* sorted by scope */
+			'@babel/plugin-proposal-throw-expressions',
+			'@babel/plugin-proposal-do-expressions',
+			'@babel/plugin-proposal-async-do-expressions',
 			[
 				'@babel/plugin-proposal-discard-binding',
 				{
 					syntaxType: 'void',
 				},
 			],
-			'@babel/plugin-proposal-throw-expressions',
-			'@babel/plugin-proposal-async-do-expressions',
-			'@babel/plugin-proposal-do-expressions',
-			'module:@jsp/plugin-chained-comparisons',
 			[
 				'@babel/plugin-proposal-pipeline-operator',
 				{
