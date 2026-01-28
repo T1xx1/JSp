96f6317dc53eb7f59b7984d3091bc7a7b6f3cbd5
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-01-28 20:44:23 +0100

    feat(jsp). warn when no files are detected with glob pattern

--- packages/jsp/src/compiler/compiledir.ts
+++ packages/jsp/src/compiler/compiledir.ts
@@ -1,8 +1,9 @@
 import { globSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs';
 import { dirname } from 'node:path';
+import { exit } from 'node:process';
 
 import type { CompleteConfig } from '../config/index.js';
-import { joinCwd } from '../utils/index.js';
+import { joinCwd, log } from '../utils/index.js';
 
 import { compile } from './compile.js';
 
@@ -11,8 +12,17 @@ export const compileDir = (config: CompleteConfig) => {
 		exclude: ['node_modules'],
 	}).filter((f) => f.endsWith('.jsp'));
 
+	if (filenames.length === 0) {
+		log.warn('MKYEZ40S6C', 'No JS+ inputs found with current `include` configuration');
+
+		exit();
+	}
+
 	for (const filename of filenames) {
-		const emitPath = joinCwd(config.compiler.emitDir, filename.replace('.jsp', config.compiler.emitLang === 'TypeScript' ? '.ts' : '.js'));
+		const emitPath = joinCwd(
+			config.compiler.emitDir,
+			filename.replace('.jsp', config.compiler.emitLang === 'TypeScript' ? '.ts' : '.js'),
+		);
 
 		mkdirSync(dirname(emitPath), { recursive: true });
 
