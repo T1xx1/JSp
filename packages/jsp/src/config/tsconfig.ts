3147ad49e20bd95f90858d3f54de7a56a8b74d94
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-01-29 01:09:47 +0100

    feat(jsp): tsconfig

---
+++ packages/jsp/src/config/tsconfig.ts
@@ -0,0 +1,42 @@
+import {
+	ModuleDetectionKind,
+	ModuleKind,
+	ModuleResolutionKind,
+	ScriptTarget,
+	type CompilerOptions,
+} from 'typescript';
+
+export const tsconfig: {
+	compilerOptions: CompilerOptions;
+} = {
+	compilerOptions: {
+		/* parser */
+		skipLibCheck: true,
+		alwaysStrict: true /* ECMAScript */,
+		strict: true /* TypeScript */,
+		module: ModuleKind.ESNext,
+		moduleResolution: ModuleResolutionKind.Bundler,
+		moduleDetection: ModuleDetectionKind.Force,
+		resolveJsonModule: true /* JSON */,
+		/* type checking */
+		exactOptionalPropertyTypes: true,
+		noImplicitAny: true,
+		noImplicitThis: true,
+		noUncheckedIndexedAccess: true,
+		noUncheckedSideEffectImports: true,
+		/* linter */
+		allowUnreachableCode: false,
+		allowUnusedLabels: false,
+		noImplicitOverride: true,
+		noPropertyAccessFromIndexSignature: true,
+		noUnusedLocals: true,
+		noUnusedParameters: true,
+		verbatimModuleSyntax: true,
+		/* compiler */
+		target: ScriptTarget.ESNext,
+		sourceMap: true,
+		declaration: true,
+		declarationMap: true,
+		outDir: './dist',
+	},
+};
