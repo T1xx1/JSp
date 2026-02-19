e1c67f11cac6006a1626f7defd5d6011de307d9e
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-02-19 15:01:01 +0100

    change(polyfill-promise-ispromise): refactor

--- packages/polyfill-promise-ispromise/src/index.ts
+++ packages/polyfill-promise-ispromise/src/index.ts
@@ -1,13 +1,19 @@
 declare global {
-	interface Promise<T> {
-		isPromise: (object: object) => boolean;
+	interface PromiseConstructor {
+		/**
+		 * Returns true if the given value is a thenable (Promises/A+ compatible).
+		 * Note: this checks for duck-typed thenables, not strictly Promise instances.
+		 * 
+		 * @see https://jsplang.vercel.app/language/polyfill/promiseispromise
+		 */
+		isPromise(value: unknown): value is PromiseLike<unknown>;
 	}
 }
 
-Promise.isPromise = (object) => {
+Promise.isPromise = function isPromise(value): value is PromiseLike<unknown> {
 	return (
-		!!object &&
-		(typeof object === 'object' || typeof object === 'function') &&
-		typeof object.then === 'function'
+		!!value &&
+		(typeof value === 'object' || typeof value === 'function') &&
+		typeof (value as Record<string, unknown>).then === 'function'
 	);
 };
