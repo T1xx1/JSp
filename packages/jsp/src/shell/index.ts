385909ed17284c534c1fa9bee5064aa6c1cb0446
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-02-21 11:25:25 +0100

    fix(create-jsp): rm root package json

--- packages/jsp/src/shell/index.ts
+++ packages/jsp/src/shell/index.ts
@@ -13,9 +13,6 @@ import { getRuntime } from './utils.js';
 
 const SCRIPT_ROOT = fileURLToPath(import.meta.url);
 
-const ROOT_PACKAGE_JSON = JSON.parse(
-	readFileSync(join(SCRIPT_ROOT, '..', '..', '..', '..', '..', 'package.json'), 'utf8'),
-);
 const SCRIPT_PACKAGE_JSON = JSON.parse(
 	readFileSync(join(SCRIPT_ROOT, '..', '..', '..', 'package.json'), 'utf8'),
 );
@@ -43,7 +40,7 @@ shell
 			`${chalk.hex('#3178C6')('TypeScript')} ${SCRIPT_PACKAGE_JSON.devDependencies['typescript'].replaceAll('^', '')}`,
 		);
 		console.log(`${chalk.hex('#F0DB4F')('JavaScript')} ESNext ESM`);
-		console.log(`${chalk.hex('#84B464')('Node')}       ${ROOT_PACKAGE_JSON.engines.node}`);
+		console.log(`${chalk.hex('#84B464')('Node')}       ${SCRIPT_PACKAGE_JSON.engines.node}`);
 	});
 
 /*  */
