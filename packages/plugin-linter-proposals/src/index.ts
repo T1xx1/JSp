08d9e3ec8d9c689da38762fb1d767b48fa1999b1
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-02-23 21:57:37 +0100

    feat(plugin-linter-proposals): async do expressions

--- packages/plugin-linter-proposals/src/index.ts
+++ packages/plugin-linter-proposals/src/index.ts
@@ -1,10 +1,28 @@
 import { types, type NodePath, type PluginPass } from '@babel/core';
-import type { VariableDeclarator } from '@babel/types';
+import type { CallExpression, VariableDeclarator } from '@babel/types';
 
 export default function ({ types: t }: { types: typeof types }) {
 	return {
 		name: '@jsplang/plugin-linter-proposals',
 		visitor: {
+			CallExpression(path: NodePath<CallExpression>, state: PluginPass) {
+				/* async do expressions */
+				if (
+					t.isFunctionExpression(path.node.callee) ||
+					t.isArrowFunctionExpression(path.node.callee)
+				) {
+					/* @ts-expect-error */
+					state.file.ast.errors.push({
+						type: 'Error',
+						category: 'Semantic',
+						message: 'Prefer `async do` expressions over IIFEs',
+						loc: {
+							line: path.node.loc?.start.line!,
+							column: path.node.loc?.start.column!,
+						},
+					});
+				}
+			},
 			VariableDeclarator(path: NodePath<VariableDeclarator>, state: PluginPass) {
 				/* do expressions */
 				if (t.isConditionalExpression(path.node.init)) {
